9f493aff78f8d6224dbdb3cf227e3b0bd33c2f0b
Luke Harris <lukeha@gmail.com>
2017-01-10 22:45:50 +0000

    Add search with Observables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import './rxjs-extensions';
+
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -13,13 +15,15 @@ import { AppRoutingModule } from './app-routing.module';
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hero-search/hero-search.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+
+import { HeroSearchService } from '../hero-search.service';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-search',
+  templateUrl: './hero-search.component.html',
+  styleUrls: ['./hero-search.component.css'],
+  providers: [HeroSearchService]
+})
+export class HeroSearchComponent implements OnInit {
+  heroes: Observable<Hero[]>;
+  // a Subject is a producer of an observable event stream
+  private searchTerms = new Subject<string>();
+
+  constructor(
+    private HeroSearchService: HeroSearchService,
+    private router: Router) { }
+
+  // each call to search puts a new string into
+  // this subject's observable stream 
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  // A Subject is also an Observable. We're going to turn
+  // the stream of search terms into a stream of Hero arrays
+  // and assign the result to the heroes property
+  ngOnInit(): void {
+    this.heroes = this.searchTerms
+      .debounceTime(300) // wait for 300ms pause in events
+      .distinctUntilChanged() // ignore if next search term is same as prev
+      .switchMap(term => term // switch to new observable each time
+        // return the http search observable
+        ? this.HeroSearchService.search(term)
+        // or the observable of empty heroes if no search term
+        : Observable.of<Hero[]>([]))
+      .catch(error => {
+        console.log(error);
+        return Observable.of<Hero[]>([]);
+      });
+  }
+
+  gotoDetail(hero: Hero): void {
+    let link = ['detail', hero.id];
+    this.router.navigate(link);
+  }
+}
